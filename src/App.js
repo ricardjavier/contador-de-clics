ed61a79c2d1d57bc62b0eda845f9726adee3cb5b
ricardjavier <rmantilla00@gmail.com>
2023-12-19 12:20:48 -0300

    Css Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import freecodecamplogo from './imagenes/freecodecamp-logo.png';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= 'freecodecamp-logo-contenedor' >
+        <img 
+          className='freecodecamp-logo'
+          src={freecodecamplogo}
+          alt='Logo de freecodecamp'/>
+      </div>
     </div>
   );
 }
