e0a9a506569421845db34ffaf68376c30235840f
ricardjavier <rmantilla00@gmail.com>
2023-12-20 16:53:39 -0300

    estilos de Botones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import freecodecamplogo from './imagenes/freecodecamp-logo.png';
-
+import Boton from './componentes/boton';
 
 
 
 function App() {
+
+const manejarClic = () => {
+  console.log('Clic');
+}
+  const reiniciarContador = () => {
+    console.log('reiniciar');
+  }
+
   return (
     <div className="App">
       <div className= 'freecodecamp-logo-contenedor' >
@@ -14,6 +22,17 @@ function App() {
           src={freecodecamplogo}
           alt='Logo de freecodecamp'/>
       </div>
+      <div className='contenedor-principal'>
+        <Boton 
+        texto='clic'
+        esBotonDEClic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Reiniciar'
+        esBotonDEClic={false}
+        manejarClic={reiniciarContador}/>
+        
+      </div> 
     </div>
   );
 }
